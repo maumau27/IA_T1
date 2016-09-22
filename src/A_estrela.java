fbdcacdcb6e800964126278bfa69b8d3ac628931
maumau27 <mauriciolana@hotmail.com>
2016-09-22 19:44:58 -0300

    Commit: -Ajustes no A* e Corrente_Celula -Criação do ArController

--- src/A_estrela.java
+++ src/A_estrela.java
@@ -16,8 +16,9 @@ public class A_estrela{
 	private Corrente_Celula ccelula_atual;
 	private ArrayList<Corrente_Celula> ccelulas_planejadas;
 	private ArrayList<Corrente_Celula> ccaminho;
+	private Encontros encontros;
 
-	public A_estrela(int clareiras_esperadas, Celula ponto_inicial, Celula ponto_final, Mapa floresta){
+	public A_estrela(int clareiras_esperadas, Celula ponto_inicial, Celula ponto_final, Mapa floresta, Encontros encontros){
 		this.clareiras_esperadas = clareiras_esperadas;
 		this.clareiras_passadas = 0;
 		this.ponto_inicial = ponto_inicial;
@@ -25,43 +26,45 @@ public class A_estrela{
 		this.floresta = floresta;
 		this.ccelula_atual = null;
 		this.ccelulas_planejadas = new ArrayList<Corrente_Celula>();
+		this.ccaminho = new ArrayList<Corrente_Celula>();
 		this.ccelulas_planejadas.add(new Corrente_Celula(ponto_inicial, Heuristica(ponto_inicial)));
+		this.encontros = encontros;
 	}
 	
 	private void Iterar(){
-		this.ccelula_atual = this.ccelulas_planejadas.get(0);
-		this.ccelulas_planejadas.remove(this.ccelula_atual);
-		this.ccaminho.add(this.ccelula_atual);
+		this.ccelula_atual = this.ccelulas_planejadas.get(0);//da um passo
+		this.ccelulas_planejadas.remove(this.ccelula_atual);//remove celula usada da lista de celulas planejadas
+		this.ccaminho.add(this.ccelula_atual);//e coloca na de passadas
+		this.ccelula_atual.ObterPai().DesTornarFolha();//faz com que o pai da celula atual pare de ser folha
 		
-		if(this.ccelula_atual.ObterCelula().custo == -1)
-			this.clareiras_passadas++;
+		if(this.ccelula_atual.ObterCelula().custo == -1)//caso seja um clareira
+			this.clareiras_passadas++;//aumenta o contador
 
-		ArrayList<Celula> celulas_vizinhas = this.floresta.ObterVizinhos(this.ccelula_atual.ObterCelula());
+		ArrayList<Celula> celulas_vizinhas = this.floresta.ObterVizinhos(this.ccelula_atual.ObterCelula());//pega as casas vizinhas
 		
-		for (Celula celula : celulas_vizinhas) {
-			this.ccelulas_planejadas.add(new Corrente_Celula(celula, this.ccelula_atual, Heuristica(celula)));
+		for (Celula celula : celulas_vizinhas) {//para cada casa vizinha
+			this.ccelulas_planejadas.add(new Corrente_Celula(celula, this.ccelula_atual, Heuristica(celula), this.encontros.ObterCustoEncontro(this.clareiras_passadas)));//adiciona na lista de celulas planejadas
 		}
-		
-		//sort celulas_planejadas
-		this.ccelulas_planejadas.sort(Corrente_Celula.Comparar_Custo);
+
+		this.ccelulas_planejadas.sort(Corrente_Celula.Comparar_Custo);//organiza a lista de celula planejadas, para a melhor celula ficar na frente
 	}
 	
 	private void DesIterar(){
-		ArrayList<Celula> celulas_vizinhas = this.floresta.ObterVizinhos(this.ccelula_atual.ObterCelula());
+		ArrayList<Celula> celulas_vizinhas = this.floresta.ObterVizinhos(this.ccelula_atual.ObterCelula());//obtem os vizinhos
 		
-		for (Corrente_Celula elemento : this.ccelulas_planejadas) {
-			if(celulas_vizinhas.contains(elemento.ObterCelula()) && elemento.ObterPai() == this.ccelula_atual){
-				this.ccelulas_planejadas.remove(elemento);
+		for (Corrente_Celula elemento : this.ccelulas_planejadas) {//para cada celula planejada
+			if(celulas_vizinhas.contains(elemento.ObterCelula()) && elemento.ObterPai() == this.ccelula_atual){//se ela estiver na lista de celulas vizinhas
+				this.ccelulas_planejadas.remove(elemento);//remove a celula planejada
 			}
 		}
 		
-		this.ccelulas_planejadas.add(this.ccelula_atual);
-		this.ccaminho.remove(this.ccelula_atual);
+		this.ccelulas_planejadas.add(this.ccelula_atual);//coloca a celula atua nas planejadas
+		this.ccaminho.remove(this.ccelula_atual);//remove do caminho
 		
-		this.ccelula_atual = this.ccelula_atual.ObterPai();
+		this.ccelula_atual = this.ccelula_atual.ObterPai();//torna a celula atual seu pai
+		this.ccelula_atual.TornarFolha();//torna a celula atual uma folha
 		
-		//sort celulas_planejadas
-		this.ccelulas_planejadas.sort(Corrente_Celula.Comparar_Custo);
+		this.ccelulas_planejadas.sort(Corrente_Celula.Comparar_Custo);//organiza a lista de celula planejadas, para a melhor celula ficar na frente
 	}
 	
 	public int DarPasso(){
@@ -72,19 +75,6 @@ public class A_estrela{
 			this.Iterar();
 			return 0;
 		}
-	}
-	
-	public int DarPasso(int n_passos){
-		for (int i = 0; i < n_passos; i++) {	
-			if(this.ccelula_atual.ObterCelula().x == this.ponto_final.x && this.ccelula_atual.ObterCelula().y == ponto_final.y){
-				return 1;
-			}
-			else{
-				this.Iterar();
-				
-			}
-		}
-		return 0;
 	}
 	
 	public int VoltarPasso(){
@@ -98,22 +88,6 @@ public class A_estrela{
 			this.DesIterar();
 			return 0;
 		}
-	}
-	
-	public int VoltarPasso(int n_passos){
-		for (int i = 0; i < n_passos; i++) {
-			if(this.ccelula_atual == null){
-				return -1;
-			}
-			else if(this.ccelula_atual.ObterCelula().x == this.ponto_inicial.x && this.ccelula_atual.ObterCelula().y == ponto_inicial.y){
-				return 1;
-			}
-			else{
-				this.DesIterar();
-				
-			}
-		}
-		return 0;
 	}
 	
 	public Corrente_Celula ObterCelulaAtual(){
@@ -132,7 +106,7 @@ public class A_estrela{
 		return this.ccelulas_planejadas;
 	}
 	
-	public float Heuristica(Celula casa_inicial) {
+	public double Heuristica(Celula casa_inicial) {
 		return (casa_inicial.x - this.ponto_final.x) + (casa_inicial.y - this.ponto_final.y);
 	}
 }
--- src/Corrente_Celula.java
+++ src/Corrente_Celula.java
@@ -1,22 +1,25 @@
 import java.util.Comparator;
 public class Corrente_Celula{
-		public Celula celula_atual;
-		public float custo_acumulado;//custo do caminho da cedula inicial ate a atual
-		public float custo_total;//custo acumulado + heristica(Geometria do táxi)
-		public Corrente_Celula celula_pai;
+		private Celula celula_atual;
+		private double custo_acumulado;//custo do caminho da cedula inicial ate a atual
+		private double custo_total;//custo acumulado + heristica(Geometria do táxi)
+		private Corrente_Celula celula_pai;
+		public boolean flag_folha;
 		
-		public Corrente_Celula(Celula celula_atual, Corrente_Celula celula_pai, float heuristica){
+		public Corrente_Celula(Celula celula_atual, Corrente_Celula celula_pai, double heuristica, double custo_encontro){
 			this.celula_atual = celula_atual;
 			this.celula_pai = celula_pai;
-			this.custo_acumulado = celula_pai.custo_acumulado + celula_atual.custo;
+			this.custo_acumulado = celula_pai.custo_acumulado + ((celula_atual.custo > 0) ? celula_atual.custo : custo_encontro) ;
 			this.custo_total = this.custo_acumulado + heuristica;
+			this.flag_folha = true;
 		}
 		
-		public Corrente_Celula(Celula celula_atual, float heuristica){
+		public Corrente_Celula(Celula celula_atual, double heuristica){
 			this.celula_atual = celula_atual;
 			this.celula_pai = null;
 			this.custo_acumulado = 0;
 			this.custo_total = this.custo_acumulado + heuristica;
+			this.flag_folha = true;
 		}
 		
 		public Corrente_Celula ObterPai(){
@@ -27,18 +30,30 @@ public class Corrente_Celula{
 			return this.celula_atual;
 		}
 		
-		public float ObterCustoAcumulado(){
+		public double ObterCustoAcumulado(){
 			return this.custo_acumulado;
 		}
 		
-		public float ObterCustoTotal(){
+		public double ObterCustoTotal(){
 			return this.custo_total;
+		}
+		
+		public boolean HeFolha(){
+			return this.flag_folha;
+		}
+		
+		public void TornarFolha(){
+			this.flag_folha = true;
+		}
+		
+		public void DesTornarFolha(){
+			this.flag_folha = false;
 		}
 		
 		public static Comparator<Corrente_Celula> Comparar_Custo = new Comparator<Corrente_Celula>() {
 			public int compare(Corrente_Celula c1, Corrente_Celula c2){
-				float custo_c1 = c1.ObterCustoTotal();
-				float custo_c2 = c2.ObterCustoTotal();
+				double custo_c1 = c1.ObterCustoTotal();
+				double custo_c2 = c2.ObterCustoTotal();
 				
 				return (int)(custo_c1-custo_c2);
 			}
